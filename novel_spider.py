134820534e1b8d1cd63eb87788d6b0d4d9283899
augustine <hcz123@qq.vip.qq>
2018-05-08 21:54:03 +0800

    add

---
+++ novel_spider.py
@@ -0,0 +1,29 @@
+
+import re
+import requests
+import time
+
+headers = {
+	'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
+}
+
+f = open('/Users/augustinehuang/Desktop/docq.txt','a+')
+
+def get_info(url):
+	res = requests.get(url,headers=headers)
+	if res.status_code == 200:
+		contents = re.findall('<p>(.*?)</p>',res.content.decode('utf-8',"ignore"),re.S)
+		for content in contents:
+			f.write(content+'\n')
+		else:
+			pass
+
+if __name__ == '__main__':
+	urls = ['http://www.doupoxs.com/doupocangqiong/{}.html'.format(str(i)) for i in range(2,1665)]
+	for url in urls:
+		get_info(url)
+		time.sleep(1)
+
+f.close()
+
+
